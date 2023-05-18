fe16fff727b07d0a86402e8d6398472a95933658
juanvissermini <juanvissermini@gmail.com>
2023-05-18 15:02:49 +0200

    Update 1

--- main.py
+++ main.py
@@ -1,30 +1,65 @@
 import openai
-#from dotenv.main import load_dotenv
 import os
 
-#load_dotenv(".env")
-on["OPENAI_API_KEY"] = "YOUR_APIKEY_HERE"
+# Set your OpenAI API key
+os.environ["OPENAI_API_KEY"] = "YOUR API KEY HERE"
 
 # Create OpenAI API instance
 openai.api_key = os.environ["OPENAI_API_KEY"]
 
-# Define your prompt
-prompt = "Hello, I'm a chatbot. What can I help you with today?"
+# Load your dataset from a text file
+dataset_file = "my_dataset.txt"
+
+def load_dataset():
+    with open(dataset_file, "r") as f:
+        return f.read()
+
+def save_dataset(dataset_text):
+    with open(dataset_file, "w") as f:
+        f.write(dataset_text)
+
+# Load the existing dataset
+dataset_text = load_dataset()
+
+# Read the set of journals
+journals_file = "journals.txt"
+
+def load_journals():
+    with open(journals_file, "r") as f:
+        return f.readlines()
+
+journals = load_journals()
+
+# Define your prompt and append the dataset text
+prompt = f"Hello, I'm a chatbot. What can I help you with today?\n{dataset_text}"
 
 while True:
-        user_input = input("You: ")
+    user_input = input("You: ")
 
-        # Generate a response from the OpenAI API
-        prompt = user_input
+    # Check if the user input matches any journal
+    if any(journal.strip().lower() in user_input.lower() for journal in journals):
+        # Perform actions for reading a journal
+        # Example: Fetch the journal content and provide a response
+        response = "Sure! Let me fetch the journal for you."
+
+    else:
+        # Generate a response using the prompt
+        prompt = f"{prompt}\nYou: {user_input}"
         response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt=prompt,
-        max_tokens=50,
-        n=1,
-        stop=None,
-        temperature=0.5,
-        #object: "model-id-2",
+            engine="text-davinci-002",
+            prompt=prompt,
+            max_tokens=50,
+            n=1,
+            stop=None,
+            temperature=0.5,
         )
 
-        # Print the response
-        print(response.choices[0].text.strip())
+        # Get the chatbot's response
+        response_text = response.choices[0].text.strip()
+
+        # Append the user input and chatbot response to the dataset
+        dataset_text += f"\nUser: {user_input}\nChatbot: {response_text}"
+        save_dataset(dataset_text)
+
+        # Print the chatbot's response
+        print(f"Chatbot: {response_text}")
